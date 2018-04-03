78b1a9ec1b0df63957f8cfa6de1fff473d40acf8
yoyo <galialiga@163.com>
2018-04-03 10:20:21 +0800

    classify

---
+++ src/Components/Classify/Sort/index.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+import axios from 'axios'
+import './index.css'
+
+class Sort extends Component{
+	constructor(props) {
+		super(props);
+		
+	}
+	render(){
+		return <div>
+			sort
+		</div>
+	}
+}
+
+export default Sort
--- src/Router/index.js
+++ src/Router/index.js
@@ -9,6 +9,8 @@ import App from '../App.js'
 import Home from '../Components/Home/index.js'
 import Cart from '../Components/Cart/index.js'
 import Classify from '../Components/Classify/index.js'
+import Sort from '../Components/Classify/Sort/index.js'
+import Brands from '../Components/Classify/Brands/index.js'
 import My from '../Components/My/index.js'
 import Login from '../Components/Login/index.js'
 import Register from '../Components/Register/index.js'
@@ -20,7 +22,15 @@ const router = (
 		<App>
 			<Switch>
 				<Route path="/home" component={Home}/>
-				<Route path="/classify" component={Classify}/>
+				<Route path="/classify" render={()=>
+					<Classify>
+						<Switch>
+							<Route path="/classify/sort" component={Sort}/>
+							<Route path="/classify/brands" component={Brands}/>
+							<Redirect from="/" to="/classify/sort"/>
+						</Switch>
+					</Classify>
+				}/>
 				<Route path="/cart" component={Cart}/>
 				<Route path="/my" render={(props)=>{
 					console.log(props)
